0c9e25b19f6e415d21cd80f66867f8499bdf70ec
TaohuaX <1140622616@qq.com>
2019-06-25 19:30:32 +0800

    添加before()、after()、方法 进行单元测试

--- expresscurd/test/userservice.test.js
+++ expresscurd/test/userservice.test.js
@@ -4,6 +4,15 @@ const assert = require('assert'); // 断言库
 describe('我是第一个测试用例', function () {
     // 定义测试用例     测试fetchUsers方法是否达到我们的预期
     it('#fetchUsers()', function () {
+        // 初始化场景的数据    所有测试用例执行之前执行的代码
+        before(function(){
+            console.log('wo是先执行');
+            require('../initdata.js');   // 初始化数据
+        });
+        // 清除测试数据     所有测试用例执行完成后执行的代码
+        after(function(){
+            console.log('我是最后执行的');
+        });
         // 这个方法返回一个数组
         var arr = userService.fetchUsers();
         assert.equal(true, Array.isArray(arr));
