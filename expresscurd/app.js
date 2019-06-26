1af9ebdf23c315865ac91987048e25f22c5e8215
TaohuaX <1140622616@qq.com>
2019-06-26 15:41:38 +0800

    用户列表的增删改查、bulma样式的使用

--- expresscurd/app.js
+++ expresscurd/app.js
@@ -3,11 +3,15 @@ const path = require('path');
 const art_express = require('express-art-template');
 const app = express();
 const userServie = require('./service/userservice.js');
+const bodyParser = require('body-parser');
+const multer = require('multer');
 // 设置art-template的模板引擎
 app.engine('art',  art_express);
 // 提供静态目录的中间件
 app.use(express.static(path.join(__dirname, 'public')));
-// 动态请求
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// 动态请求         获取全部的数据
 app.get('/user/list', (req, res) => {
         // 默认从根目录下  的views文件夹下搜索
                 // render方法第一个参数是文件名
@@ -17,7 +21,29 @@ app.get('/user/list', (req, res) => {
                 users: userServie.fetchUsers()
         })
 })
-app.listen(4002, () => {
+// 动态请求             分页获取数据
+app.get('/user/pagelist', (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const size = parseInt(req.query.size) || 20;
+    const data = userServie.fetchPageUsers(page, size) || [];
+    res.render('userlist1.art', {
+        title: '你好啊--分页!',
+        users: data
+    })
+})
+// 添加用户
+app.post('/user/add', (req, res) => {
+    const data = userServie.addUsers(req.body.id,  req.body.username);
+    // const data = userServie.addUsers(req.body.id,  req.body.username);
+    res.redirect('/user/list');
+})
+// 删除用户
+app.post('/user/del', (req, res) => {
+    userServie.delUsers(req.body.username);
+    // const data = userServie.addUsers(req.body.id,  req.body.username);
+    res.redirect('/user/list');
+})
+app.listen(4000, () => {
         console.log(__dirname);
         console.log('启动成功');
-});
+})
--- expresscurd/service/userservice.js
+++ expresscurd/service/userservice.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const dbjson = require('../db.json');
 // 获取所有的用户数据
 exports.fetchUsers = function () {
@@ -7,5 +9,39 @@ exports.fetchUsers = function () {
 // page 第几页
 // size 一页多少条
 exports.fetchPageUsers = function (page, size) {
-
+    let start = page * size - size;
+    let end = page * size;
+    let data = dbjson.users.slice(start, end);
+    return data;
+}
+// 获取用户数据
+// id ID
+// name 用户名
+exports.addUsers = function (id, name) {
+    dbjson.users.push({'id': id, 'name': name})
+    fs.writeFileSync(path.join(__dirname, '../db.json'), JSON.stringify({users: dbjson.users}), {
+        encoding: 'utf8'
+    });
+    return  {
+        msg: '保存成功',
+        code: 1
+    }
+}
+// 删除用户信息
+// name : 当前的name
+exports.delUsers = (name) => {
+    dbjson.users.forEach((item, index) => {
+        if (item.name === name) {
+            
+            dbjson.users.splice(index, 1);
+            console.log(dbjson.users[index])
+            // fs.writeFileSync(path.join(__dirname, '../db.json'), JSON.stringify({users: dbjson.users}), {
+            //     encoding: 'utf8'
+            // });
+            return  {
+                msg: '保存成功',
+                code: 1
+            }
+        }
+    });
 }
